01ab50b7428fd433886d95ab961b86eb246ac4f8
Callum Prentice <callum@lindenlab.com>
2026-08-11 14:45:52 -0700

    Rename the batch script to indicate it now only creates consumers - the producer is started as the default project from Visual Studio

---
+++ tools/start_consumers.bat
@@ -0,0 +1,15 @@
+@echo off
+
+set N=2
+
+if "%~1"=="" (
+    echo No argument was provided - using default consumer count
+) else (
+    echo Argument found! You passed: %~1
+    set N=%~1
+)
+
+for /L %%i in (1, 1, %N%) do (
+    echo Starting consunmer: %%i
+    start build\release\llshmframe_cef_consumer.exe
+)
--- tools/test_cef.bat
+++
@@ -1,4 +0,0 @@
-start build\release\llshmframe_cef_producer.exe
-
-start build\release\llshmframe_cef_consumer.exe
-start build\release\llshmframe_cef_consumer.exe
